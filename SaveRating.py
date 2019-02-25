d10e098128036b830315e347ae8b5e12618ca9b2
Demid Kucherenko <kucherenkodemid@gmail.com>
2019-02-25 11:32:03 +0300

    add rating5

--- SaveRating.py
+++ SaveRating.py
@@ -14,7 +14,7 @@ def main():
     api = CodeforcesAPI()
 
     users = get_users(api)
-    save_ratings_to_file(users, 'rating4.txt')
+    save_ratings_to_file(users, 'rating5.txt')
 
 
 if __name__ == '__main__':
