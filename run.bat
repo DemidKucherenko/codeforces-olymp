45d6f008824a2568efe33cf2882497e49891ed75
PCMS-server-FML31 <fml31-summercamp@yandex.ru>
2019-04-23 18:46:35 +0500

    add rating 6

--- run.bat
+++ run.bat
@@ -7,8 +7,8 @@
 		rename tmp.2.html codeforces-upsolving2.html	
 		del codeforces-upsolving3.html
 		rename tmp.3.html codeforces-upsolving3.html	
-rem		del codeforces-upsolving4.html
-rem		rename tmp.4.html codeforces-upsolving4.html	
+		del codeforces-upsolving4.html
+		rename tmp.4.html codeforces-upsolving4.html	
 rem		del codeforces-upsolving5.html
 rem		rename tmp.5.html codeforces-upsolving5.html	
 rem		del tmp.6.html
